a254ea5607eda449a779e5e13ab997b99a93d47e
Lianderson <liandersonr18@gmail.com>
2025-05-05 13:40:56 -0300

    feat:instala o prisma e as rotas que precisa

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,109 @@
-const user = [
-    {id:1,nome:"jose",email:"j@gmail.com"}
-]
+import { PrismaClient } from "@prisma/client";
 
-export const getAllUsers = (req,res)=>{
-    res.status(200).send((user));
+const prisma =new PrismaClient();
+
+export const getAllUsers = async (req,res)=>{
+    try {
+        const users = await prisma.usuario.findMany()
+        res.status(200).send((users));
+    } catch (error) {
+        res.status(500).send(error)
+    }
 }
 
-export const postUser = (req,res)=>{
-    const { NovoNome,NovoEmail } = req.body
-    const idNovo=user.length+1
-    user.push({id:idNovo,nome:NovoNome,email:NovoEmail})
-    res.status(201).send(`O usuário ${NovoNome} com o email ${NovoEmail} foi criado!!!`)    
+export const createUser = async (req,res)=>{
+    try{
+        const { NovoNome,NovoEmail } = req.body
+        const novoUsuario = await prisma.usuario.create({
+            data:{
+                nome:NovoNome,
+                email:NovoEmail
+            },
+        })
+        res.status(201).send(`O usuário ${NovoNome} com o email ${NovoEmail} foi criado!!!`)    
+    }catch(error){
+        res.status(501).send(`erro ao cadastrar: código ${error.code}`)
+    }
+}
+
+export const updateUser = async (req,res)=>{
+    try{
+        const id = parseInt(req.params.id);
+        const { NovoNome,NovoEmail } = req.body
+        const usuarioAtualizado = await prisma.usuario.update({
+            where:{id:id},
+            data:{
+                nome:NovoNome,
+                email:NovoEmail
+            },
+        })
+        res.status(201).send(usuarioAtualizado)    
+    }catch(error){
+        res.status(501).send(`erro ao Atualizar:código ${error.code}`)
+    }
+}
+
+export const deleteUser = async (req,res)=>{
+    try{
+        const id = parseInt(req.params.id);
+        const usuarioDeletado = await prisma.usuario.delete({
+            where:{id}
+        })
+        res.status(204).send(usuarioDeletado)    
+    }catch(error){
+        res.status(501).send(`erro ao deletar:código ${error.code}`)
+    }
+}
+
+export const getUsersFromId = async (req,res)=>{
+    try {
+        const id = parseInt(req.params.id);
+        const users = await prisma.usuario.findUnique({where:{id}})
+        res.status(200).send((users));
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+export const getAllUsersFromName= async (req,res)=>{
+    try {
+        const { nome } = req.params;        
+        const users = await prisma.usuario.findMany({where:{nome}})
+        res.status(200).send((users));
+    } catch (error) {
+        res.status(500).send(error)
+
+    }
 }
 
 
+export const getAllUsersFromComecoName= async (req,res)=>{
+    try {
+        const { comeco } = req.params;   
+        const users = await prisma.usuario.findMany({
+            where: {
+                  nome: {
+                    startsWith: comeco,
+                  },
+            },
+        });        
+        res.status(200).send((users));
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+// buscar todos os usuarios entre o valor min e max.
+export const idMinMax = async (req, res) => {
+    const min = parseInt(req.body.min)
+    const max = parseInt(req.body.max)
+    const users = await prisma.usuario.findMany({
+        where: {
+            id: {
+                gte: min,
+                lte: max,
+            },
+        },
+    });
+
+    res.send(users);
+}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getAllUsers, postUser } from "../controllers/userController.js"
+import { getAllUsers, createUser, updateUser, deleteUser, getUsersFromId, getAllUsersFromName, getAllUsersFromComecoName, idMinMax} from "../controllers/userController.js"
 const router = express.Router();
 
 router.get("/", getAllUsers)
-router.post("/", postUser)
+router.post("/", createUser)
+router.put("/:id", updateUser)
+router.delete("/:id", deleteUser)
+router.get("/id/:id", getUsersFromId)
+router.get("/nome/:nome", getAllUsersFromName)
+router.get("/comeco_nome/:comeco", getAllUsersFromComecoName)
+router.get("/entre/",idMinMax)
 
 export default router;
